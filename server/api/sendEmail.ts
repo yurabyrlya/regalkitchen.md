5646e44a570f1940b4a03089bf3c41f033615608
iuri <iuribirlea@utilita.co.uk>
2024-12-22 21:15:33 +0000

    Create sendEmail API endpoint

--- server/api/sendEmail.ts
+++ server/api/sendEmail.ts
@@ -1,43 +1,27 @@
-import nodemailer from 'nodemailer';
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default defineEventHandler(async (event) => {
+  const msg = {
+    to: 'iuribirlea@gmail.com', // Change to your recipient
+    from: 'contact@regalkitchen.md', // Change to your verified sender
+    subject: 'Contact Request',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+
   try {
-    // Parse the incoming form data
-    //const body = await readBody(event);
-    const { name, email, message } = {
-        name : 'Name',
-        email : 'yurabyrlya1@gmail.com',
-        message : 'Test Meesage',
-    };
-
-    // Create a nodemailer transporter
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail', // Or any other email service (e.g., SendGrid, Outlook, SMTP)
-      auth: {
-        user: 'iuribirlea@gmail.com', // Replace with your email
-        pass: 'oowkeximgvsjhayb', // Replace with your app-specific password
-      },
-    });
-
-    // Define the email options
-    const mailOptions = {
-      from: 'regalkitch3n@gmail.com', // The sender's email
-      to: 'iuribirlea@gmail.com', // The recipient (your email)
-      subject: `New Contact Form Submission from ${name}`,
-      text: `
-        Name: ${name}
-        Email: ${email}
-        Message: ${message}
-      `,
-    };
-
-    // Send the email
-    await transporter.sendMail(mailOptions);
-
-    // Return success response
-    return { success: true, message: 'Email sent successfully!' };
+    await sgMail.send(msg);
+    return { success: true, message: 'Email sent successfully' };
   } catch (error) {
     console.error('Error sending email:', error);
-    return { success: false, message: 'Failed to send email.' };
+    throw createError({
+      statusCode: 500,
+      message: 'Failed to send email',
+    });
   }
 });
