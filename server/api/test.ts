1d6326c90bd48e9ae4d1fd99d76523f3f3a1db5a
iuri <iuribirlea@utilita.co.uk>
2024-12-27 20:44:13 +0000

    test api

--- server/api/test.ts
+++ server/api/test.ts
@@ -2,17 +2,8 @@ import sgMail from '@sendgrid/mail';
 
 export default defineEventHandler(async (event) => {
   //const body = await readBody(event); // Read the request body for POST data.
-  return {
-    statusCode: 200,
-    message: 'Ok',
-  };
+
   // Ensure POST method is used.
-  if (event.node.req.method !== 'POST') {
-    return {
-      statusCode: 200,
-      message: 'Only POST requests are allowed',
-    };
-  }
 
   // Set the SendGrid API key.
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -22,8 +13,8 @@ export default defineEventHandler(async (event) => {
       to: 'iuribirlea@gmail.com', // Replace with your recipient
       from: 'test@regalkitchen.md', // Replace with your verified sender
       subject: 'Sending with SendGrid via Nuxt Nitro',
-      text: body.text || 'Hello from Nuxt Nitro!',
-      html: body.html || '<strong>Hello from Nuxt Nitro!</strong>',
+      text:  'Hello from Nuxt Nitro!',
+      html: '<strong>Hello from Nuxt Nitro!</strong>',
     };
 
     await sgMail.send(msg);
