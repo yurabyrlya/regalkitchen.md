97ce98cee016f291d2028353ddf28c249ce7fb90
iuri <iuribirlea@utilita.co.uk>
2024-12-29 20:18:02 +0000

    Remove unused functions

--- functions/api/test.js
+++
@@ -1,6 +0,0 @@
-export function onRequestGet(context){
-    return new Response(
-      JSON.stringify({ success: true, message: JSON.stringify(context) }),
-      { status: 200, headers: { 'Content-Type': 'application/json' } }
-    );
-};
